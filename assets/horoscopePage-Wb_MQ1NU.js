49cf46ca97e71754ef9a34693ee43355315a0297
Андрей Андронов <andrew270504cska@gmail.com>
2024-08-21 22:58:26 +0300

    added dist

---
+++ assets/horoscopePage-Wb_MQ1NU.js
@@ -0,0 +1,1 @@
+import{h as o}from"./index-Dj1avTu3.js";const u={__name:"horoscopePage",setup(t){const e=o();return console.log(e.query,localStorage.getItem("userLang")),(r,s)=>null}};export{u as default};
