623bcd904f14e4f385d3879b65647a4797e990c2
Андрей Андронов <andrew270504cska@gmail.com>
2024-08-22 20:25:54 +0300

    small fix

--- src/pages/mainPage.vue
+++ src/pages/mainPage.vue
@@ -28,7 +28,7 @@ function selectSignAndDate(date){
    <div v-if="!isOptionModal" class="flex flex-col items-center w-full">
       <p class="font-semibold mb-4">{{ $t('main_header') }}</p>
       <div class="grid grid-cols-2 gap-3 px-4">
-         <div class="relative flex justify-around items-center border-2 rounded-lg border-black dark:bg-white/45 dark:text-black p-px hover:scale-110 transition-transform duration-300 ease-in-out" v-for="(sign, index) in words[lang].zodiac_signs" :key="index" @click="selectSign(index)">
+         <div class="relative cursor-pointer flex justify-around items-center border-2 rounded-lg border-black dark:bg-white/45 dark:text-black p-px hover:scale-110 transition-transform duration-300 ease-in-out" v-for="(sign, index) in words[lang].zodiac_signs" :key="index" @click="selectSign(index)">
             <img :src="sign.img" class="w-5/12" :alt="`zodiac ${sign.name} icon`">
             <p class="">{{ sign.name }}</p>
          </div>
@@ -36,8 +36,8 @@ function selectSignAndDate(date){
    </div>
    <div v-if="isOptionModal">
       <div class="flex flex-col items-center gap-3">
-         <p class="p-2 bg-white/75 rounded-lg dark:text-black hover:scale-110 transition-transform duration-300 ease-in-out" v-for="(option, index) in words[lang].zodiac_options" :key="index" @click="selectSignAndDate(index)">{{ option }}</p>
-         <p class="p-1 bg-white/50 rounded-lg mt-4 dark:text-black" @click="isOptionModal = false">{{ $t('back_button') }}</p>
+         <p class="p-2 cursor-pointer bg-white/75 rounded-lg dark:text-black hover:scale-110 transition-transform duration-300 ease-in-out" v-for="(option, index) in words[lang].zodiac_options" :key="index" @click="selectSignAndDate(index)">{{ option }}</p>
+         <p class="p-1 bg-white/50 cursor-pointer rounded-lg mt-4 dark:text-black" @click="isOptionModal = false">{{ $t('back_button') }}</p>
       </div>
    </div>
 </template>
