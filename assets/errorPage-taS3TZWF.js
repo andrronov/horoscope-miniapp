d3dfebe5ce03028458c584ef361b5511dbaa5eea
andrronov <andrew270504cska@gmail.com>
2025-11-12 22:42:37 +0500

    Updates

---
+++ assets/errorPage-taS3TZWF.js
@@ -0,0 +1,1 @@
+import{S as t}from"./index.es-5uH89rwv.js";import{c as a,a as o,t as s,i as n,b as m,F as i,o as l}from"./index-5XQ-xns6.js";const p={class:"fon-semibold p-2 bg-red-500 text-white"},f={__name:"errorPage",setup(c){return(e,r)=>(l(),a(i,null,[o("h1",p,s(e.$t("error_msg")),1),n(m(t),{onClick:r[0]||(r[0]=u=>e.$router.push({name:"main"}))})],64))}};export{f as default};
--- assets/horoscopePage-Wb_MQ1NU.js
+++
@@ -1,1 +0,0 @@
-import{h as o}from"./index-Dj1avTu3.js";const u={__name:"horoscopePage",setup(t){const e=o();return console.log(e.query,localStorage.getItem("userLang")),(r,s)=>null}};export{u as default};
